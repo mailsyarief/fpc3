61fbf05d56f33180ccaa9da7b80ccf74e8ae5dbd
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-12-03 15:12:54 +0700

    Add files via upload

---
+++ touch.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]){
+	int f;
+	if(argc != 2){
+	printf(2, "need 2 arguments\n");
+	}
+	if((f = open(argv[1], O_CREATE)) < 0){
+		printf(1, "touch: %s failed to create\n", argv[1]);
+	}
+	
+	exit();
+}
