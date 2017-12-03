d9f71dafc3deb00267db6c4937f2a5b25eb20480
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-12-03 19:23:46 +0700

    udah bisa dipake, tp ccd

--- join.c
+++ join.c
@@ -3,9 +3,21 @@
 #include "user.h"
 #include "fcntl.h"
 
+int length(char *input){
+    int length = 0;
+    while(input[length]!='\0')  
+    {
+        length++;
+    }
+    return length;
+}
+
 int main(int argc, char *argv[]){
 	int n1, n2, f1, f2;
 	char buf1[500], buf2[500], hasil[500];
+	//printf(1, "%s\n", argv[1]);
+	//printf(1, "%s\n", argv[2]);
+	
 	if((f1 = open(argv[1], O_RDONLY)) < 0){
 		printf(1, "failed to read %s\n", argv[1]);
 		exit();
@@ -13,6 +25,31 @@ int main(int argc, char *argv[]){
 	if((f2 = open(argv[2], O_RDONLY)) < 0){
 		printf(1, "failed to read %s\n", argv[2]);
 		exit();
+	}
+	int counter1 = 0, counter2 = 0, counter = 0;
+	n1 = read(f1, buf1, sizeof(buf1));
+	n2 = read(f2, buf2, sizeof(buf2));
+	//printf(2, "%d\n", n1);
+	//printf(2, "%d\n", n2);
+	int p1, p2;
+	p1 = length(buf1);
+	p2 = length(buf2);
+	if(n1 > 0 || n2 > 0){
+		//printf(1, "masuk ke if\n");
+		while(p1-- || p2--){	
+		//printf(1, "masuk ke while\n");
+			while(buf1[counter1] != '\n'){
+				hasil[counter] = buf1[counter1];
+				counter++; counter1++;
+			}			
+			while(buf2[counter2] != '\n'){
+				hasil[counter] = buf2[counter2];
+				counter++; counter2++;
+			}
+			hasil[counter] = '\n';
+			counter++; counter1++; counter2++;
+		//printf(1, "%s\n", hasil);
+		}
 	}
 	close(f1);
 	close(f2);
