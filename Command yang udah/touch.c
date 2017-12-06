85b40d2e7a80166347c1af0f564ab567e3f1130a
Unknown <ismail8syarief@gmail.com>
2017-12-06 19:35:33 +0700

    touch nya jadi bisa bikin banyak file @ a time

--- Command yang udah/touch.c
+++ Command yang udah/touch.c
@@ -2,15 +2,26 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
+#include "syscall.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+	
 	int f;
-	if(argc != 2){
-	printf(2, "need 2 arguments\n");
+	if(argc < 2)
+	{
+		printf(2, "need 2 arguments\n");
 	}
-	if((f = open(argv[1], O_CREATE | O_RDWR)) < 0){
-		printf(1, "touch: %s failed to create\n", argv[1]);
+
+for(int i=0;i<=argc;i++)
+{
+	if((f = open(argv[i], O_CREATE | O_RDWR)) < 0)
+	{
+		printf(1, "touch: %s failed to create\n", argv[i]);
 	}
+
 	close(f);
+}	
 	exit();
 }
+
