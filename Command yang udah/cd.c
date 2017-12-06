519ac94bc6f9762c12bf9cf02901346366076b64
Unknown <ismail8syarief@gmail.com>
2017-12-06 19:36:22 +0700

    cd

---
+++ Command yang udah/cd.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]){
+  //validasi input
+  if(argc<2)
+  {
+    printf(1,"need 2 arguments\n");
+    exit();
+  }  
+  //cd
+  if(chdir(argv[1])<0)
+  {
+    printf(1,"Failed to change directory\n");
+  }
+  
+  exit();
+}
+
